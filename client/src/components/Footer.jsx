b8b3c0e492c7910a66b341d7d7c0929b1d3ee272
Kashish <kashishpatel2934@gmail.com>
2023-08-28 20:38:17 +0530

    add header and footer components

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,78 @@
+import '../css/Homepage.css';
+
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer container-fluid">
+      <div className="row">
+          <div className="col-lg-6">
+              <div className="contact-form">
+                  <form className="form-horizontal" action="" method="post">
+                      <fieldset>
+                          <legend className="footer-text-contact footer-text-contact-extra">Contact us</legend>
+
+                          <div className="form-group">
+                              <label className="col-md-3 control-label" htmlFor="name">Name</label>
+                              <div className="col-md-9">
+                                  <input id="name" name="name" type="text" placeholder="Your name"
+                                      className="form-control" />
+                              </div>
+                          </div>
+
+                          <div className="form-group">
+                              <label className="col-md-3 control-label" htmlFor="email">Your E-mail</label>
+                              <div className="col-md-9">
+                                  <input id="email" name="email" type="text" placeholder="Your email"
+                                      className="form-control" />
+                              </div>
+                          </div>
+
+                          <div className="form-group">
+                              <label className="col-md-3 control-label" htmlFor="message">Your message</label>
+                              <div className="col-md-12">
+                                  <textarea className="form-control" id="message" name="message"
+                                      placeholder="Please enter your message here..." rows="5"></textarea>
+                              </div>
+                          </div>
+
+                          <div className="form-group">
+                              <div className="col-md-12 text-right">
+                                  <button type="submit" className="btn btn-primary btn-lg submit-btn" >Submit</button>
+                              </div>
+                          </div>
+                      </fieldset>
+                  </form>
+              </div>
+          </div>
+          <div className="col-lg-3">
+              <div className="sitemap">
+                  <div className="sitemap-txt">
+                      <legend className='sitemap-txt-extra'>Sitemap</legend>
+
+                      <p>All our pages</p>
+
+                      <ul>
+                          <li><a href="https://kashish2934.github.io/Home-Page/">Home</a></li>
+                          <li><a href="https://kashish2934.github.io/Home-Page/">About us</a></li>
+                          <li><a href="https://kashish2934.github.io/Home-Page/">Features</a></li>
+                          <li><a href="https://kashish2934.github.io/Home-Page/">Pricing</a></li>
+                          <li><a href="https://kashish2934.github.io/Home-Page/">Contact us</a></li>
+                      </ul>
+                  </div>
+              </div>
+          </div>
+
+
+          <div className="col-lg-3 ">
+              <div className="contact-details">
+                  <div className="contact-details-text">
+                      <legend className='footer-text-contact-extra'>Mobile No </legend>
+                      <p>+91 0123456789</p>
+                      <legend className='footer-text-contact-extra'>Email </legend>
+                      <p>abc@daiict.ac.in</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
   )
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,44 @@
+import { Link } from 'react-router-dom';
+import '../css/Homepage.css';
+
 export const Header = () => {
   return (
-    <div>Header</div>
+    <div className="main-container">
+        <nav className="navbar navbar-expand-lg navbar-light">
+            <div className="container-fluid">
+                <a className="navbar-brand nav-text" href="#">Xpense Trackr</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link nav-text" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link nav-text" href="#">About us</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link nav-text" href="#">Features</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link nav-text" href="#">Purchase</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link nav-text" href="#">Contact Us</a>
+                        </li>
+                    </ul>
+
+                    <form className="d-flex" role="search">
+                        <Link to='/login'><button className="btn btn-primary" id="log-in">Log in</button></Link>
+                        <Link to='/register'><button className="btn btn-secondary" id="sign-up">Sign Up</button></Link>
+                    </form>
+
+                </div>
+            </div>
+        </nav>
+      </div>
   )
 }
