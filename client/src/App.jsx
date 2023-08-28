05349bf5bbb46e40ff8615d2d053f339aac1110a
Govrani Keyur <202101498@daiict.ac.in>
2023-08-28 19:31:18 +0530

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+import {HashRouter as Router, Routes, Route} from 'react-router-dom'
+import {Home} from './pages/Home'
+import {Register} from './pages/Register'
+import {Login} from './pages/Login'
+import {Header} from './components/Header'
+import {Footer} from './components/Footer'
+
+
+function App() {
+  
+  return (
+    <>
+      <div className='text-center'>
+      <Router>
+
+        <Header />
+        
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+        
+        <Footer />
+
+        
+      </Router>
+    </div>
+    </>
+  )
+}
+
+export default App
