228a5b8d093dcddfc8b5d4fc85116412d4955958
Govrani Keyur <202101498@daiict.ac.in>
2023-09-13 15:01:09 +0530

    modified header and footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,78 +1,142 @@
-import '../css/Homepage.css';
+/* eslint-disable react/jsx-no-target-blank */
+import { MDBFooter } from "mdb-react-ui-kit";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import MailIcon from "@mui/icons-material/Mail";
+import BusinessIcon from "@mui/icons-material/Business";
+import PhoneIcon from "@mui/icons-material/Phone";
+import YouTubeIcon from "@mui/icons-material/YouTube";
 
 export const Footer = () => {
-    return (
-        <div className="footer container-fluid bg-dark">
-            <div className="row justify-content-start">
-                <div className="col-lg-5">
-                    <div className="contact-form">
-                        <form className="form-horizontal" action="" method="post">
-                            <fieldset>
-                                <legend className="footer-text-contact footer-text-contact-extra">Contact us</legend>
-
-                                <div className="form-group">
-                                    <label className="control-label" htmlFor="name">Name</label>
-                                    <div className="">
-                                        <input id="name" name="name" type="text" placeholder="Your name"
-                                            className="form-control" />
-                                    </div>
-                                </div>
-
-                                <div className="form-group">
-                                    <label className="control-label" htmlFor="email">Your E-mail</label>
-                                    <div className="">
-                                        <input id="email" name="email" type="text" placeholder="Your email"
-                                            className="form-control" />
-                                    </div>
-                                </div>
-
-                                <div className="form-group">
-                                    <label className="control-label" htmlFor="message">Your message</label>
-                                    <div className="">
-                                        <textarea className="form-control" id="message" name="message"
-                                            placeholder="Please enter your message here..." rows="5"></textarea>
-                                    </div>
-                                </div>
-
-                                <div className="form-group">
-                                    <div className="text-right">
-                                        <button type="submit" className="btn btn-primary btn-sm submit-btn" >Submit</button>
-                                    </div>
-                                </div>
-                            </fieldset>
-                        </form>
-                    </div>
-                </div>
-                <div className="col-lg-4">
-                    <div className="sitemap">
-                        <div className="sitemap-txt">
-                            <legend className='sitemap-txt-extra'>Sitemap</legend>
-
-                            <p>All our pages</p>
-
-                            <ul className='px-0'>
-                                <ul className='py-1'><a className='text-decoration-none link-secondary' href="https://kashish2934.github.io/Home-Page/">Home</a></ul>
-                                <ul className='py-1'><a className='text-decoration-none link-secondary' href="https://kashish2934.github.io/Home-Page/">About us</a></ul>
-                                <ul className='py-1'><a className='text-decoration-none link-secondary' href="https://kashish2934.github.io/Home-Page/">Features</a></ul>
-                                <ul className='py-1'><a className='text-decoration-none link-secondary' href="https://kashish2934.github.io/Home-Page/">Pricing</a></ul>
-                                <ul className='py-1'><a className='text-decoration-none link-secondary' href="https://kashish2934.github.io/Home-Page/">Contact us</a></ul>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-
-
-                <div className="col-lg-3">
-                    <div className="contact-details">
-                        <div className="contact-details-text">
-                            <legend className='footer-text-contact-extra'>Mobile No </legend>
-                            <p>+91 0123456789</p>
-                            <legend className='footer-text-contact-extra'>Email </legend>
-                            <p>abc@daiict.ac.in</p>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <MDBFooter bgColor="dark" className="text-white">
+      <section className="px-5">
+        <div className="container-fluid">
+          <div className="row d-flex align-items-center">
+            <div className="col-6 mt-3">
+              <div className="mx-3 display-6">Follow us</div>
             </div>
+
+            <div className="col-6 mt-3 text-end">
+              <div>
+                <a
+                  href="https://github.com/Keyur1284/Group26_Software_Project"
+                  target="_blank"
+                >
+                  <button
+                    className="btn btn-lg btn-dark rounded-circle text-white"
+                    style={{ height: "75px", marginLeft: "100px" }}
+                  >
+                    <GitHubIcon sx={{ fontSize: 40 }} />
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/Keyur1284/Group26_Software_Project"
+                  target="_blank"
+                >
+                  <button
+                    className="btn btn-lg btn-dark rounded-circle text-white"
+                    style={{ height: "75px" }}
+                  >
+                    <LinkedInIcon sx={{ fontSize: 40 }} />
+                  </button>
+                </a>
+                <a href="https://github.com/Keyur1284/Group26_Software_Project">
+                  <button
+                    className="btn btn-lg btn-dark rounded-circle text-white"
+                    style={{ height: "75px" }}
+                  >
+                    <YouTubeIcon sx={{ fontSize: 40 }} />
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/Keyur1284/Group26_Software_Project"
+                  target="_blank"
+                >
+                  <button
+                    className="btn btn-lg btn-dark rounded-circle text-white"
+                    style={{ height: "75px" }}
+                  >
+                    <TwitterIcon sx={{ fontSize: 40 }} />
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/Keyur1284/Group26_Software_Project"
+                  target="_blank"
+                >
+                  <button
+                    className="btn btn-lg btn-dark rounded-circle text-white"
+                    style={{ height: "75px" }}
+                  >
+                    <InstagramIcon sx={{ fontSize: 40 }} />
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/Keyur1284/Group26_Software_Project"
+                  target="_blank"
+                >
+                  <button
+                    className="btn btn-lg btn-dark rounded-circle text-white"
+                    style={{ height: "75px" }}
+                  >
+                    <FacebookIcon sx={{ fontSize: 40 }} />
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+      </section>
+
+      <section className="p-5">
+        <div className="container-fluid">
+          <div className="row d-flex">
+            <div className="col-4">
+              <p className="display-6">
+                <a
+                  href="https://github.com/Keyur1284/Group26_Software_Project"
+                  target="_blank"
+                >
+                  <img
+                    src="https://static.dezeen.com/uploads/2023/07/x-logo-twitter-elon-musk_dezeen_2364_col_0-1-600x600.jpg"
+                    alt="logo"
+                    className="rounded-circle me-3"
+                    style={{ height: "50px", width: "50px" }}
+                  />
+                </a>
+                Xpense Tracker
+              </p>
+              <p>
+                Welcome to Xpense Track, where we revolutionize the way
+                businesses manage and track employee expenses.
+              </p>
+            </div>
+
+            <div className="col-4"></div>
+
+            <div className="col-4">
+              <div className="container mx-3">
+                <p className="display-6 mb-4">Contact Us</p>
+                <p>
+                  <BusinessIcon className="me-2" />
+                  Gujarat, India
+                </p>
+                <p>
+                  <MailIcon className="me-2" /> 20210xxxx@daiict.ac.in
+                </p>
+                <p>
+                  <PhoneIcon className="me-2" /> +91 9876543210
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="text-center bg-dark p-2">Copyright © Group26 2023</div>
+    </MDBFooter>
+  );
+};
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,39 +3,49 @@ import '../css/Homepage.css';
 
 export const Header = () => {
     return (
-        <div className="navbar navbar-expand-lg navbar-dark bg-dark navbar-custom">
-            <div className="container-md">
-                <a className="text-decoration-none text-white link-secondary mx-2" href="#">XpenseTrackerLogo</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-2" style={{fontSize: "18px"}}>
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">Xpense Tracker</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="d-flex flex-row justify-content-between">
-                <a className="text-decoration-none text-secondary link-light mx-2" href="#">Home</a>
-                <a className="text-decoration-none text-secondary link-light mx-2" href="#">Purchase</a>
-                <a className="text-decoration-none text-secondary link-light mx-2" href="#">Products</a>
-                </div>
-                
-                <div className="collapse navbar-collapse" id="navbarNavDarkDropdown">
-                    <ul className="navbar-nav">
+
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" aria-current="page" to="/">Purchase</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" aria-current="page" to="/">Products</Link>
+                        </li>
                         <li className="nav-item dropdown">
-                            <a className="nav-link text-secondary link-light dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a className="nav-link text-secondary link-light dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Resources
-                            </a>
+                        </a>
                             <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
-                                <li><a className="dropdown-item" href="#">User Guide</a></li>
-                                <li><a className="dropdown-item" href="#">FAQs</a></li>
-                                <li><a className="dropdown-item" href="#">Articles</a></li>
+                                <li>
+                                    <Link className="dropdown-item" to="/">User Guide</Link>
+                                </li>
+                                <li>
+                                    <Link className="dropdown-item" to="/">FAQs</Link>
+                                </li>
+                                <li>
+                                    <Link className="dropdown-item" to="/">Articles</Link>
+                                </li>
                             </ul>
                         </li>
+            
+                        <li className="nav-item">
+                            <Link className="nav-link" aria-current="page" to="/">About Us</Link>
+                        </li>
                     </ul>
+                    <Link to='/login'><button className="btn btn-success mx-3" type="submit" style={{fontSize: "18px"}}>Login</button></Link>
+                    <Link to='/register'><button className="btn btn-primary" type="submit" style={{fontSize: "18px"}}>Sign Up</button></Link>
                 </div>
             </div>
-
-            <form className="d-flex flex-row justify-content-end" role="search">
-                <Link to='/login'><button className="custom-btn btn btn-sm btn-primary" id="log-in">Log in</button></Link>
-                <Link to='/register'><button className="custom-btn btn btn-sm btn-secondary mx-2" id="sign-up">Sign Up</button></Link>
-            </form>
-
-        </div >
+        </nav>
     )
 }
