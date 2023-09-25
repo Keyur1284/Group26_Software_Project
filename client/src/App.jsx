f84f670bc24b2fdad1958a97f7cdbbfd51270422
Nancy Patel <nancypatel5757@gmail.com>
2023-09-25 21:46:44 +0530

    added project dashboard route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import {Register} from './pages/Register'
 import {Login} from './pages/Login'
 import {Header} from './components/Header'
 import {Footer} from './components/Footer'
+import { ProjectDashboard } from './pages/ProjectDashboard'
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/register' element={<Register />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/project/dashboard' element={<ProjectDashboard />} />
         </Routes>
         
         <Footer />
