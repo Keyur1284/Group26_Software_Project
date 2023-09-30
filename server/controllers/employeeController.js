c643f8de3b3ef3414f807d8b22f20ab235ec552c
Govrani Keyur <202101498@daiict.ac.in>
2023-09-30 15:38:44 +0530

    add invitation controllers

--- server/controllers/employeeController.js
+++ server/controllers/employeeController.js
@@ -1,4 +1,6 @@
 const Employee = require('../models/employeeModel');
+const Invite = require('../models/inviteModel');
+const Project = require('../models/projectModel');
 const asyncHandler = require('express-async-handler');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
@@ -96,9 +98,73 @@ const loginController = asyncHandler(async (req, res) => {
 });
 
 
+const getInvitesController = asyncHandler(async (req, res) => {
+   
+    const employee_id = req.employee._id;
+    const invitations = await Invite.find({employee_id});
+    
+    if (invitations.length > 0)
+    {
+        res.status(200).json({
+            success: true,
+            invitations
+        });
+    }
+
+    else
+    {
+        res.status(200).json({
+            success: true,
+            message: "No invitations found"
+        });
+    }
+});
+
+
+const acceptInviteController = asyncHandler(async (req, res) => {
+
+    const {invite_id} = req.body;
+    const employee_id = req.employee._id;
+    const invite = await Invite.findById(invite_id);
+    const project = await Project.findById(invite.project_id);
+
+    if (invite && project)
+    {
+        const employee = await Employee.findById(employee_id);
+        
+        employee.projects.push(project._id);
+        await employee.save();
+        
+        project.employees.push(employee._id);
+        await project.save();
+        
+        await Invite.findByIdAndDelete(invite_id);
+        
+        res.status(200).json({
+            success: true,
+            employee,
+            project,
+            message: "Invite accepted"
+        });
+    }
+
+    else
+    {
+        res.status(400)
+        // res.json({success: false, message: 'Invalid invite'});
+        throw new Error('Invalid invite');
+    }
+})
+
+
 const generateToken = (_id) => {
     return jwt.sign({_id}, process.env.JWT_SECRET, {expiresIn: '30d'});
 }
 
 
-module.exports = {loginController, registerController};
+module.exports = {
+    loginController, 
+    registerController,
+    getInvitesController,
+    acceptInviteController
+};
--- server/routes/employeeRoute.js
+++ server/routes/employeeRoute.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {loginController, registerController} = require('../controllers/employeeController');
+const {loginController, registerController, getInvitesController, acceptInviteController} = require('../controllers/employeeController');
+const {employeeAuthMiddleware} = require('../middlewares/authMiddleware');
 
 router.post('/register', registerController);
 router.post('/login', loginController);
+router.get('/get-invites', employeeAuthMiddleware, getInvitesController);
+router.post('/accept-invite', employeeAuthMiddleware, acceptInviteController);
 
 module.exports = router;
