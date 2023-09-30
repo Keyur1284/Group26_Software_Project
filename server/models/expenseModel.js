4e8dccae182c522bfadf7c150eac4561bfcfad74
error-path <202101475@daiict.ac.in>
2023-09-30 14:21:40 +0530

    add expense Model

---
+++ server/models/expenseModel.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+const expenseSchema = new mongoose.Schema({
+    
+    name: {
+        type: String,
+        required: [true, 'Please enter your name'],
+        trim: true
+    },
+    date: {
+        type: Date,
+        required: [true, 'Please enter the date'],
+        trim: true
+    },
+    category: {
+        type: String,
+        enum: {
+            values: ['Fuel', 'Equipment', 'Food', 'Office', 'Maintenance', 'Rent', 'Taxes', 'Travel', 'Internet', 'Other'],
+            message: 'Category is invalid!'
+        },
+        required: [true, 'Please enter the category']
+    },
+    amount: {
+        type: Number,
+        required: [true, 'Please enter the amount'],
+    },
+    description: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    project_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true, 'Please enter the project id'],
+        ref: 'Project'
+    },
+    employee_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true, 'Please enter the employee id'],
+        ref: 'Employee'
+    },
+    status: {
+        type: String,
+        enum: {
+            values: ['Pending', 'Approved', 'Rejected'],
+            default: 'Pending',
+            message: 'Status is invalid!'
+        }
+    },
+    file: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Uploads', 
+        required: [true, 'Please provide a file'],
+    }
+    
+}, {
+    timestamps: true
+});
+
+const Expense = mongoose.model('Expense', expenseSchema);
+module.exports = Expense;
