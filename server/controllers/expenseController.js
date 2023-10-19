1ca469458119901655fa631993394729045bd92e
error-path <202101475@daiict.ac.in>
2023-10-19 19:25:55 +0530

    add update expense controller

--- server/controllers/expenseController.js
+++ server/controllers/expenseController.js
@@ -5,7 +5,7 @@ const asyncHandler = require('express-async-handler');
 const addExpenseController = asyncHandler(async (req, res) => {
 
     const {name, date, category, amount, description, file} = req.body;
-    const project_id = req.params.id;
+    const project_id = req.params.project_id;
     const employee_id = req.employee._id;
 
 
@@ -79,7 +79,78 @@ const addExpenseController = asyncHandler(async (req, res) => {
 
 });
 
+const updateExpenseController = asyncHandler(async (req, res) => {
+
+    const {name, date, category, amount, file} = req.body;
+    const expense_id = req.params.expense_id;
+
+    if(!name || !date || !category || !amount || !file)
+    {
+        res.status(400)
+        // res.json({success: false, message: 'Please fill all the fields'});
+        throw new Error('Please fill all the fields');
+    }
+
+    if (amount <= 0)
+    {
+        res.status(400)
+        // res.json({success: false, message: 'Amount must be greater than 0'});
+        throw new Error('Amount must be greater than 0');
+    }
+
+    let currentDate = new Date();
+    const istOffset = 330 * 60000;
+    currentDate = new Date(currentDate.getTime() + istOffset);
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth() + 1;
+    const currentDay = currentDate.getDate();
+
+    const dateArray = date.split('-');
+    const year = parseInt(dateArray[0]);
+    const month = parseInt(dateArray[1]);
+    const day = parseInt(dateArray[2]);
+
+    if (year > currentYear)
+    {
+        res.status(400)
+        throw new Error('Date must be less than or equal to current date');
+    }
+
+    if (year === currentYear && month > currentMonth)
+    {
+        res.status(400)
+        throw new Error('Date must be less than or equal to current date');
+    }
+
+    if (year === currentYear && month === currentMonth && day > currentDay)
+    {
+        res.status(400)
+        throw new Error('Date must be less than or equal to current date');
+    }
+
+    const updatedExpense = await Expense.findByIdAndUpdate(expense_id, req.body, {new: true});
+
+    if (updatedExpense)
+    {
+        res.status(200).json({
+            success: true,
+            updatedExpense
+        });
+    }
+
+    else
+    {
+        res.status(400)
+        // res.json({success: false, message: 'Invalid data'});
+        throw new Error('Invalid data');
+    }
+
+});
+
+
+
 
 module.exports = {
-    addExpenseController
+    addExpenseController,
+    updateExpenseController
 };
--- server/routes/expenseRoute.js
+++ server/routes/expenseRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { addExpenseController} = require('../controllers/expenseController');
+const {addExpenseController, updateExpenseController} = require('../controllers/expenseController');
 const {employeeAuthMiddleware} = require('../middlewares/authMiddleware');
 
-router.post('/add-expense/:id', employeeAuthMiddleware, addExpenseController);
+router.post('/add-expense/:project_id', employeeAuthMiddleware, addExpenseController);
+router.patch('/update-expense/:expense_id', employeeAuthMiddleware, updateExpenseController);
 
 module.exports = router;
