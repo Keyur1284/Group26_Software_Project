eae53ede2655b2743fb6c649b2aa048cac805645
Govrani Keyur <202101498@daiict.ac.in>
2023-10-20 16:46:16 +0530

    update employee notification model

--- server/models/employeeNotificationModel.js
+++ server/models/employeeNotificationModel.js
@@ -1,5 +1,3 @@
-// model to notify the employee about the status of the expense request if accepted or rejected by manager
-
 const mongoose = require('mongoose');
 
 const employeeNotificationSchema = new mongoose.Schema({
@@ -13,15 +11,6 @@ const employeeNotificationSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         required: [true, 'Please enter the expense id'],
         ref: 'Expense'
-    },
-    status: {
-        type: String,
-        required: false,
-        enum: {
-            values: ['Pending', 'Approved', 'Rejected'],
-            default: 'Pending',
-            message: 'Status is invalid!'
-        }
     },
     message: {
         type: String,
