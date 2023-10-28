cac00f9688a3f9161fff324ff7e712cc41625b6a
Govrani Keyur <202101498@daiict.ac.in>
2023-10-28 22:33:41 +0530

    add project state to store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice'
+import projectReducer from '../features/project/projectSlice'
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer
+        auth: authReducer,
+        project: projectReducer
     }
 })
