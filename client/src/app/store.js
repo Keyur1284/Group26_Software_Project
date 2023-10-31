638c53a89aaa7e968a22c6324cdea0a2b7cb5eb6
error-path <202101475@daiict.ac.in>
2023-10-31 21:00:27 +0530

    add announcement state

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice'
 import projectReducer from '../features/project/projectSlice'
+import announcementReducer from '../features/announcement/announcementSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
-        project: projectReducer
+        project: projectReducer,
+        announcement: announcementReducer
     }
 })
