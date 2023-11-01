854dd2313a9528e4c58f0930f138a9be44e9df6f
Govrani Keyur <202101498@daiict.ac.in>
2023-11-01 23:47:07 +0530

    update get expense controller

--- server/controllers/expenseController.js
+++ server/controllers/expenseController.js
@@ -205,21 +205,12 @@ const getExpenseEmployeeController = asyncHandler(async (req, res) => {
 
     const employee_id = req.employee._id;
     const project_id = req.params.project_id;
-    const expenses = await Expense.find({ employee_id, project_id });
+    const expenses = await Expense.find({ employee_id, project_id }).populate('employee_id', 'firstName lastName');
 
-    if (expenses.length > 0) {
-        res.status(200).json({
-            success: true,
-            expenses
-        });
-    }
-
-    else {
-        res.status(200).json({
-            success: true,
-            message: "No expenses found!"
-        });
-    }
+    res.status(200).json({
+        success: true,
+        expenses
+    });
 });
 
 
