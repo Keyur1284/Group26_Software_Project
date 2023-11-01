a0019d7ce01ccbe9cb654658f0da2b1054c5aff9
Govrani Keyur <202101498@daiict.ac.in>
2023-11-01 14:03:20 +0530

    add invite state to store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -3,12 +3,14 @@ import authReducer from '../features/auth/authSlice'
 import projectReducer from '../features/project/projectSlice'
 import announcementReducer from '../features/announcement/announcementSlice';
 import teamReducer from '../features/team/teamSlice';
+import inviteReducer from '../features/invite/inviteSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         project: projectReducer,
         announcement: announcementReducer,
-        team: teamReducer
+        team: teamReducer,
+        invite: inviteReducer
     }
 })
