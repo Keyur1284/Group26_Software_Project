18b3927298ae5a0767fa997f4a6b42b1789e0816
error-path <202101475@daiict.ac.in>
2023-11-02 23:36:16 +0530

    integrate deleting notifications with backend

--- client/src/pages/NotificationManager.jsx
+++ client/src/pages/NotificationManager.jsx
@@ -1,34 +1,11 @@
 import "bootstrap/dist/css/bootstrap.css";
 import CircleRoundedIcon from "@mui/icons-material/CircleRounded";
 import mainbg from "../assets/project-dashboard/main-bg.jpg";
-import { useDispatch , useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { getManagerNotifications, reset } from "../features/notification/notificationSlice";
+import { getManagerNotifications, deleteNotificationManager, reset } from "../features/notification/notificationSlice";
 
 export const NotificationManager = () => {
-  const notification_data = [
-    {
-      employeeName: "John Doe",
-      text: " has added a new expense in ",
-      projectName: "Project A",
-      time:"9:30 am",
-      expName: "Expense 1",
-    },
-    {
-      employeeName: "Jane Doe",
-      text: " has added a new expense in ",
-      projectName: "Project B",
-      time:"9:30 am",
-      expName: "Expense 2",
-    },
-    {
-      employeeName: "Jill Doe",
-      text: " has added a new expense in ",
-      projectName: "Project C",
-      time:"9:30 am",
-      expName: "Expense 3",
-    },
-  ];
 
   const dispatch = useDispatch();
   const { notifications, isLoading, isSuccess, isError, appErr, serverErr } = useSelector((state) => state.notification);
@@ -36,12 +13,12 @@ export const NotificationManager = () => {
   useEffect(() => {
 
     dispatch(getManagerNotifications());
-    
+
   }, [dispatch]);
 
   useEffect(() => {
 
-    if(isSuccess || isError) {
+    if (isSuccess || isError) {
       dispatch(reset());
     }
 
@@ -56,61 +33,63 @@ export const NotificationManager = () => {
         minHeight: "92vh",
       }}
     >
-        <h1 className="px-4 py-4" style={{color:"#013E8F"}}>Notifications</h1>
+      <h1 className="px-4 py-4" style={{ color: "#013E8F" }}>Notifications</h1>
 
-    <div className="col-md-12 ">
-      {notifications.map((notification, index) => (
-        <div
-          key={index}
-          className="card"
-          style={{
-            margin: "10px",
-            height: "17vh",
-            borderRadius: "20px",
-            background: "linear-gradient(180deg, #5B89C8 0%, rgba(255, 255, 255, 0.00) 70%)",
-            position: "relative",
-          }}
-        >
-          <div className="card-body">
-            <h3 className="card-title px-4" style={{ color: "#00000066" }}>
-              <CircleRoundedIcon sx={{ color: "#013E8F" }} />
-              <span style={{ marginLeft: 17 }}>
-                <strong style={{ color: "black" }}>{notification.employee}</strong>
-                {" "}{notification.message}{" "}
-                <strong style={{ color: "black" }}>{notification.project_id.name}</strong>
-              </span>
-            </h3>
-          </div>
+      <div className="col-md-12 ">
+        {notifications.map((notification, index) => (
           <div
+            key={index}
+            className="card"
             style={{
-              position: "absolute",
-              top: "27px",
-              right: "47px",
-              color: "#00000080",
+              margin: "10px",
+              height: "17vh",
+              borderRadius: "20px",
+              background: "linear-gradient(180deg, #5B89C8 0%, rgba(255, 255, 255, 0.00) 70%)",
+              position: "relative",
             }}
           >
-    <h5><strong>{new Date(notification.createdAt).toLocaleString}</strong></h5>
-          </div>
-      <div className="d-flex justify-content-between">
-          <div style={{ position: "relative", bottom: "17px", left: "80px" ,display: 'inline-block'}}>
+            <div className="card-body">
+              <h3 className="card-title px-4" style={{ color: "#00000066" }}>
+                <CircleRoundedIcon sx={{ color: "#013E8F" }} />
+                <span style={{ marginLeft: 17 }}>
+                  <strong style={{ color: "black" }}>{notification.employee}</strong>
+                  {" "}{notification.message}{" "}
+                  <strong style={{ color: "black" }}>{notification.project_id.name}</strong>
+                </span>
+              </h3>
+            </div>
             <div
-              className="card px-4"
               style={{
-                backgroundColor: "#4E79B2",
-                color: "#fff",
-                borderRadius: "25px",
+                position: "absolute",
+                top: "27px",
+                right: "47px",
+                color: "#00000080",
               }}
             >
-              <h3> {notification.expense_id.name}</h3>
+              <h5><strong>{new Date(notification.createdAt).toLocaleString}</strong></h5>
+            </div>
+            <div className="d-flex justify-content-between">
+              <div style={{ position: "relative", bottom: "17px", left: "80px", display: 'inline-block' }}>
+                <div
+                  className="card px-4"
+                  style={{
+                    backgroundColor: "#4E79B2",
+                    color: "#fff",
+                    borderRadius: "25px",
+                  }}
+                >
+                  <h3> {notification.expense_id.name}</h3>
+                </div>
+              </div>
+            </div>
+            <div style={{ position: "absolute", bottom: "17px", right: "20px" }}>
+              <button className="btn text-white" style={{ borderRadius: "25px", backgroundColor: "#013E8F" }}
+              onClick={() => dispatch(deleteNotificationManager(notification._id))}
+              ><h5>Mark as read</h5></button>
             </div>
           </div>
-          </div>
-          <div style={{ position: "absolute", bottom: "17px", right: "20px" }}>
-        <button className="btn text-white" style={{ borderRadius: "25px" ,backgroundColor:"#013E8F"}}><h5>Mark as read</h5></button>
-          </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
     </div>
   );
 };
