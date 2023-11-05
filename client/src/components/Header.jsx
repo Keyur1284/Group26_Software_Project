2ad44fcb08e43e8a4d9a028ed221a2eae54377e5
Govrani Keyur <202101498@daiict.ac.in>
2023-11-05 18:17:18 +0530

    Add GitHub link to logo and fix null pointer exceptions

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import { Link, useNavigate } from "react-router-dom";
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
 import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
@@ -76,6 +77,18 @@ export const Header = () => {
       style={{ fontSize: "18px" }}
     >
       <div className="container-fluid">
+        <a
+            href="https://github.com/Keyur1284/Group26_Software_Project"
+            target="_blank"
+            className="navbar-brand"
+        >        
+              <img
+                src="https://static.dezeen.com/uploads/2023/07/x-logo-twitter-elon-musk_dezeen_2364_col_0-1-600x600.jpg"
+                alt="logo"
+                className="rounded-circle"
+                style={{ height: "35px", width: "35px" }}
+              />
+        </a>
         <Link className="navbar-brand" to="/">
           Xpense Tracker
         </Link>
@@ -112,7 +125,7 @@ export const Header = () => {
 
           {user ? (
             <div className="d-flex align-items-center">
-              {notifications.length > 0 ? (
+              {notifications?.length > 0 ? (
                 <Link to='/notifications' className="text-decoration-none">
                   <NotificationsActiveIcon className="text-light mx-2" style={{ fontSize: "30px" }} />
                 </Link>
@@ -122,7 +135,7 @@ export const Header = () => {
                 </Link>
               )}
               <Link to='/profile' className="text-decoration-none"> <div className="text-light mx-3" style={{ fontSize: "18px" }}>
-                {user.firstName} {user.lastName}
+                {user?.firstName} {user?.lastName}
               </div> </Link>
               <button
                 className="btn btn-danger"
