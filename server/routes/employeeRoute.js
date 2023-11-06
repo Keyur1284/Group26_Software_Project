9a2e6fe989491db7d86ae3c820b2e77c244a103f
error-path <202101475@daiict.ac.in>
2023-11-07 00:23:30 +0530

    Add forgot password functionality with nodemailer for employee

--- server/routes/employeeRoute.js
+++ server/routes/employeeRoute.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const {loginController, registerController, getEmployeeProfileController} = require('../controllers/employeeController');
+const {loginController, registerController, getEmployeeProfileController, forgotPasswordController, verifyIdAndTokenController, resetPasswordController} = require('../controllers/employeeController');
 const {employeeAuthMiddleware} = require('../middlewares/authMiddleware');
 
 router.post('/register', registerController);
 router.post('/login', loginController);
 router.get('/profile', employeeAuthMiddleware, getEmployeeProfileController);
+router.post('/forgot-password', forgotPasswordController);
+router.get('/reset-password/:id/:token', verifyIdAndTokenController);
+router.post('/reset-password/:id/:token', resetPasswordController);
 
 
 module.exports = router;
