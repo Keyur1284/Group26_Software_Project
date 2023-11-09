7342ef2991a184cbd8677918472c92efbeb72ea1
Govrani Keyur <202101498@daiict.ac.in>
2023-11-10 04:27:12 +0530

    Add reset password model

---
+++ server/models/resetPasswordModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const resetPasswordSchema = new mongoose.Schema({
+    
+    otp: {
+        type: String,
+        required: [true, 'OTP is required'],
+        trim: true,
+    },
+    manager_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Manager',
+        required: false
+    },
+    employee_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Employee',
+        required: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 300
+    }
+
+}, {
+    timestamps: true
+});
+
+const Reset = mongoose.model('Reset', resetPasswordSchema);
+module.exports = Reset;
