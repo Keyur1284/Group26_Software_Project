dbd166bb7d9774029a6b358c7ab1b926fa41e06a
Govrani Keyur <202101498@daiict.ac.in>
2023-11-16 18:25:15 +0530

    Fix conditional rendering

--- client/src/pages/Expense.jsx
+++ client/src/pages/Expense.jsx
@@ -137,7 +137,7 @@ export const Expense = () => {
 
   }, [dispatch, teamSuccess, teamError, teamAppErr, teamServerErr]);
 
-  if ((isLoading || teamLoading) && expenses?.length == 0 && employees?.length == 0)
+  if (isLoading || teamLoading)
   {
     return (
       <>
